a5ae879f3afd19a59e86fd129d2e6b846a828b69
hb9397 <hb9397@naver.com>
2025-02-05 02:32:35 +0900

    fix - 새로운 페이지에서 head.jsp 호출시, 메뉴 못불러오는 현상 수정(임시) - EPRExcPerRepMngtList Mapper 수행 명 검색 조건 LIKE로 변경

--- src/main/java/egovframework/com/lhb/epr/web/EPRExcPerRepMngtController.java
+++ src/main/java/egovframework/com/lhb/epr/web/EPRExcPerRepMngtController.java
@@ -1,20 +1,24 @@
 package egovframework.com.lhb.epr.web;
 
+import java.util.List;
+
 import org.egovframe.rte.fdl.property.EgovPropertyService;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springmodules.validation.commons.DefaultBeanValidator;
 
 import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
 import egovframework.com.cmm.SessionVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
 import egovframework.com.lhb.epr.service.EPRExcPerRepMngtService;
 import egovframework.com.lhb.epr.service.EPRExcPerRepMngtVO;
-import lombok.NoArgsConstructor;
+import egovframework.com.sym.mnu.mpm.service.EgovMenuManageService;
+import egovframework.com.sym.mnu.mpm.service.MenuManageVO;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -29,11 +33,13 @@ public class EPRExcPerRepMngtController {
 	
 	private final DefaultBeanValidator beanValidator;
 	
+	private final EgovMenuManageService menuManageService;
+	
 	private final EPRExcPerRepMngtService eprExcPerRepMngtService;
 	
     @RequestMapping("/EPRExcPerRepMngt.do")
-    public String selectEPRExcPerRepMngtList(@ModelAttribute("eprExcPerRepMngtVO") EPRExcPerRepMngtVO eprExcPerRepMngtVO, 
-    			ModelMap model) throws Exception {
+    public String selectEPRExcPerRepMngtList(@ModelAttribute("menuManageVO") MenuManageVO menuManageVO, 
+    		@ModelAttribute("eprExcPerRepMngtVO") EPRExcPerRepMngtVO eprExcPerRepMngtVO, ModelMap model) throws Exception {
     	
     	PaginationInfo paginationInfo = new PaginationInfo();
 		paginationInfo.setCurrentPageNo(eprExcPerRepMngtVO.getPageIndex());
@@ -51,6 +57,26 @@ public class EPRExcPerRepMngtController {
 		paginationInfo.setTotalRecordCount(totCnt);
         model.addAttribute("paginationInfo", paginationInfo);
         model.addAttribute("message", egovMessageSource.getMessage("success.common.select"));
+        
+        // 현재 리턴 페이지에서 메뉴 보이게 하려고.,,.,. 사실 jsp 급하게 해서 head.jsp 를 모든 새로운 페이지가 따로 조회하는 형태가 되버림
+        LoginVO user =
+    			(LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+
+    		// 미인증 사용자에 대한 보안처리
+        	if(user == null) {
+    			return "index";
+        	}
+
+        	menuManageVO.setTmpId(user.getId());
+        	menuManageVO.setTmpPassword(user.getPassword());
+        	menuManageVO.setTmpUserSe(user.getUserSe());
+        	menuManageVO.setTmpName(user.getName());
+        	menuManageVO.setTmpEmail(user.getEmail());
+        	menuManageVO.setTmpOrgnztId(user.getOrgnztId());
+        	menuManageVO.setTmpUniqId(user.getUniqId());
+
+    		List<?> list_headmenu = menuManageService.selectMainMenuHead(menuManageVO);
+    		model.addAttribute("list_headmenu", list_headmenu);
     	
         return "egovframework/com/lhb/epr/EPRExcPerRepMngt";
     }
